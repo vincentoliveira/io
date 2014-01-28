8261b0edecd83a8ed278661590eab31dd341e843
Vincent Oliveira <vincent@oliveira.fr>
2014-01-28 01:58:06 +0100

    init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 chmod -R 0777 app/cache app/logs
+rm -rf app/cache/*
 
 php app/console assets:install web --symlink
 php app/console assetic:dump
@@ -7,4 +8,5 @@ php app/console doctrine:database:drop --force
 php app/console doctrine:database:create
 php app/console doctrine:schema:create
 php app/console doctrine:fixtures:load --append --purge-with-truncate
+
 
