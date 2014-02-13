f94d3c76218015ebf5e75fe2bea0d59de19fef6e
Vincent Oliveira <vincent@oliveira.fr>
2014-02-13 17:53:20 +0100

    test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ php app/console cache:clear --env=test
 if [ "$1" == "cc" ]
 then
     echo "Test avec couverture de code"
-    phpunit --coverage-html
+    phpunit -c app/ --coverage-html=build
 else
     echo "Test sans couverture de code"
     echo "Pour activer la couverture: $0 cc"
