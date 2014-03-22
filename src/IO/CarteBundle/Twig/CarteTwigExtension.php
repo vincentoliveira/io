b5059eb9d91ca7d5c0515a3334df7a9a8f114463
vincentoliveira <vincent@oliveira.fr>
2014-03-22 15:01:34 +0100

    Design : Menu

--- src/IO/CarteBundle/Twig/CarteTwigExtension.php
+++ src/IO/CarteBundle/Twig/CarteTwigExtension.php
@@ -28,7 +28,16 @@ class CarteTwigExtension extends \Twig_Extension
     public function getFilters()
     {
         return array(
-            'imageLink' => new \Twig_Filter_Method($this, 'imageLinkFilter'),
+            'imageLink' => new \Twig_SimpleFilter('imageLink', array($this, 'imageLinkFilter')),
+        );
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions() {
+        return array(
+            'restaurantCategories' => new \Twig_SimpleFunction('restaurantCategories', array($this, 'restaurantCategories')),
         );
     }
 
@@ -47,6 +56,22 @@ class CarteTwigExtension extends \Twig_Extension
         $assets = $this->container->get('templating.helper.assets');
         $path = substr($imagePath, strpos($imagePath, 'web/') + 4);
         return $assets->getUrl($path);
+    }
+    
+    
+    /**
+     * Return restaurant categories
+     * 
+     * @return array
+     */
+    public function restaurantCategories() {
+        $userSv = $this->container->get('user.user_service');
+        $categorySv = $this->container->get('menu.category');
+        
+        $restaurantName = $userSv->getUser()->getRestaurant()->getName();
+        $categories = $categorySv->getRestaurantCategories($restaurantName);
+        
+        return $categories;
     }
 
     /**
