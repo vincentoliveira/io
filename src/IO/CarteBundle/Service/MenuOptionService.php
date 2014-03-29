f24a99ccb81e61c4f8500808af57e03a4b6f759b
Vincent Oliveira <vincent@oliveira.fr>
2014-03-29 17:15:42 +0100

    MenuOption test 1 => OK

---
+++ src/IO/CarteBundle/Service/MenuOptionService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace IO\CarteBundle\Service;
+
+/**
+ * MenuOption Service
+ */
+class MenuOptionService
+{
+    /**
+     * Get paginated MenuOption list
+     * 
+     * @return array
+     */
+    public function getPaginatedList()
+    {
+        return array();
+    }
+}
