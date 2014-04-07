bb7f709e1f40a2af7c5f679b22b27b192bf1c7ba
Vincent Oliveira <vincent@oliveira.fr>
2014-04-07 16:08:30 +0200

    Add restaurant

--- src/IO/AdminBundle/Controller/RestaurantController.php
+++ src/IO/AdminBundle/Controller/RestaurantController.php
@@ -3,8 +3,11 @@
 namespace IO\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use JMS\SecurityExtraBundle\Annotation\Secure;
+use IO\CarteBundle\Form\RestaurantType;
+use IO\CarteBundle\Entity\Restaurant;
 
 class RestaurantController extends Controller
 {
@@ -19,5 +22,33 @@ class RestaurantController extends Controller
     {
         $restaurants = $this->getDoctrine()->getRepository("IOCarteBundle:Restaurant")->findAll();
         return array('restaurants' => $restaurants);
+    }
+    
+    /**
+     * Admin restaurant index
+     * 
+     * @return type
+     * @Template()
+     * @Secure(roles="ROLE_ADMIN")
+     */
+    public function newAction(Request $request)
+    {
+        $restaurant = new Restaurant();
+        $form = $this->createForm(new RestaurantType(), $restaurant);
+        
+        if ($request->isMethod("POST")) {
+            $form->bind($request);
+            
+            $em = $this->getDoctrine()->getEntityManager();
+            $em->persist($restaurant);
+            $em->flush();
+            
+            $session = $this->container->get('session');
+            $session->getFlashBag()->add('success', 'Le restaurant à bien été crée.');
+            
+            return $this->redirect($this->generateUrl('admin_restaurants'));
+        }
+        
+        return array('form' => $form->createView());
     }
 }
