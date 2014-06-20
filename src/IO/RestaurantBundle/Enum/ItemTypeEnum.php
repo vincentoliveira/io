59df9eb03e773451f8e591d3231cc268951b9a58
vincentoliveira <vincent@oliveira.fr>
2014-06-20 11:52:03 +0200

    Hotfix

--- src/IO/RestaurantBundle/Enum/ItemTypeEnum.php
+++ src/IO/RestaurantBundle/Enum/ItemTypeEnum.php
@@ -11,6 +11,6 @@ class ItemTypeEnum
     const TYPE_DISH = 'DISH';
     const TYPE_OPTION_LIST = 'OPTION_LIST';
     const TYPE_OPTION = 'OPTION';
-    const TYPE_OPTION = 'MENU';
-    const TYPE_OPTION = 'MENU_CATEGORY';
+    const TYPE_MENU = 'MENU';
+    const TYPE_MENU_CATEGORY = 'MENU_CATEGORY';
 }
