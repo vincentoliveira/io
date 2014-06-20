129652554d9a22dd0310676e6c890a0a0b85d4b8
vincentoliveira <vincent@oliveira.fr>
2014-06-20 11:28:13 +0200

    Edit/delete option

--- src/IO/RestaurantBundle/Enum/ItemTypeEnum.php
+++ src/IO/RestaurantBundle/Enum/ItemTypeEnum.php
@@ -11,4 +11,6 @@ class ItemTypeEnum
     const TYPE_DISH = 'DISH';
     const TYPE_OPTION_LIST = 'OPTION_LIST';
     const TYPE_OPTION = 'OPTION';
+    const TYPE_OPTION = 'MENU';
+    const TYPE_OPTION = 'MENU_CATEGORY';
 }
